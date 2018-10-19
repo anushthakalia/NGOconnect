39304278b84d0d3bc74090f7bcdd47e012dac168
anushthakalia <anushthakalia@gmail.com>
2018-10-19 23:25:21 +0530

    Added latest DB file

--- NGOconnect.sql
+++ NGOconnect.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 15, 2018 at 07:40 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Oct 19, 2018 at 07:53 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,7 +34,7 @@ CREATE TABLE `company` (
   `password` varchar(32) NOT NULL,
   `comname` varchar(30) NOT NULL,
   `phone` int(10) NOT NULL,
-  `fk_ngo_id` int(11) NOT NULL
+  `fk_ngo_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,8 +42,9 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`comid`, `email`, `password`, `comname`, `phone`, `fk_ngo_id`) VALUES
-(1, 'raghav@amazon.com', 'ae2b1fca515949e5d54fb22b8ed95575', 'Amazon', 2147483647, 2),
-(2, 'shyam@microsoft.com', 'ae2b1fca515949e5d54fb22b8ed95575', 'Microsoft', 2147483647, 1);
+(1, '', '5f4dcc3b5aa765d61d8327deb882cf99', 'Amazon', 0, 2),
+(2, 'ani@flipkart.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Flipkart', 2147483647, 2),
+(3, 'abc@ola.in', '5f4dcc3b5aa765d61d8327deb882cf99', 'Ola', 2147483647, 1);
 
 -- --------------------------------------------------------
 
@@ -57,18 +58,21 @@ CREATE TABLE `internship_details` (
   `NGO` varchar(100) NOT NULL,
   `Company` varchar(100) DEFAULT NULL,
   `internship_descr` text NOT NULL,
-  `fk_intern_id` int(11) NOT NULL,
-  `fk_ngo_id` int(11) NOT NULL
+  `Location` varchar(50) NOT NULL,
+  `Start_date` varchar(32) NOT NULL,
+  `Duration` varchar(32) NOT NULL,
+  `Apply_by` date NOT NULL,
+  `Tags` varchar(18) NOT NULL,
+  `fk_intern_select_id` int(11) DEFAULT NULL,
+  `fk_ngo_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `internship_details`
 --
 
-INSERT INTO `internship_details` (`internship_id`, `Name`, `NGO`, `Company`, `internship_descr`, `fk_intern_id`, `fk_ngo_id`) VALUES
-(1, 'Operations', 'NASSCOM', NULL, 'NASSCOM is an apex industry association body for the 154 billion dollars Indian IT-BPM industry. NASSCOM\'s member companies are in the business of software development, software services, software products, IT-enabled/BPO services, and e-commerce. NASSCOM has been the strongest proponent of global free trade in India.\r\n\r\nNASSCOM Centre of Excellence for IoT is a flagship program being run in partnership between NASSCOM and Central Government.', 1, 1),
-(2, 'Content Writing', 'Teach For India', NULL, 'Teach For India, modeled on the lines of the famous Teach For America movement, is a nationwide campaign in pursuit of educational equity in India. Teach For India recruits exceptional college graduates and professionals to work as full-time teachers in under-resourced schools for two years. Teach For India solicits leaders who will pilot the movement against educational inequity and begets the leaders of tomorrow. The Teach For India staff works relentlessly along with the fellows to pioneer the movement. By enlisting our country\'s most promising future leaders in our work, we directly support our mission of building a movement of leaders who will end educational inequity. Ultimately, we are driven by two goals: our short-term goal of putting students on a different life path and our long-term goal of building a strong alumni base to advocate for our students and work to eliminate the educational achievement gap.', 2, 2),
-(3, 'Marketing', 'CRY', NULL, 'CRY is an Indian NGO established in 1979, with a focus on the most marginalized children, 0-18 years. We work as an enabling organization, choosing to support small, grass root organizations- currently in 18 states; 78 districts in India.\r\n\r\nOver the last 40 years, CRY has incubated 700+ organizations and impacted 22 lakh children across India. We have also helped raise 20L+ donors thereby creating small philanthropists in India. CRY addresses themes on education, health, nutrition, child marriage, child labour, foeticide, etc.', 1, 1);
+INSERT INTO `internship_details` (`internship_id`, `Name`, `NGO`, `Company`, `internship_descr`, `Location`, `Start_date`, `Duration`, `Apply_by`, `Tags`, `fk_intern_select_id`, `fk_ngo_id`) VALUES
+(1, 'Data Analyst', 'Goonj', 'Amazon', 'Data Analyst dolor sit amet, consectetur adipisicing elit. Officiis laborum repellat quisquam, illo soluta, deleniti, nesciunt illum eveniet tempore sed necessitatibus eligendi rerum. Cum maxime, aliquam incidunt voluptatem enim eum.\r\n\r\nHarum rerum totam eum facere sint, molestiae porro illum recusandae dolorum cumque ut illo cum quisquam esse consequatur commodi fugit culpa explicabo vero vel quos. Minus sed quod est, similique!\r\n\r\nQui id ad ipsum quis molestiae, velit, aut. Suscipit ex sunt incidunt cupiditate ad quisquam excepturi provident voluptate nemo earum, vel est facilis commodi ullam natus quis praesentium cum asperiores.', 'New Delhi', 'Immediatly', '3 months', '2018-10-24', 'Data', NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -83,7 +87,7 @@ CREATE TABLE `ngo` (
   `ngoname` varchar(30) NOT NULL,
   `address` varchar(50) NOT NULL,
   `regno` varchar(40) NOT NULL,
-  `fk_comid` int(11) NOT NULL
+  `fk_comid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -91,8 +95,9 @@ CREATE TABLE `ngo` (
 --
 
 INSERT INTO `ngo` (`ngoid`, `email`, `password`, `ngoname`, `address`, `regno`, `fk_comid`) VALUES
-(1, 'lorem.ipsum@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', 'CRY', 'test', '7777777777', 1),
-(2, 'ipsum.lorem@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', 'TFI', 'test', '9999999999', 2);
+(1, '', '5f4dcc3b5aa765d61d8327deb882cf99', 'Goonj', '', '', 1),
+(2, 'abc@goonj.in', '5f4dcc3b5aa765d61d8327deb882cf99', 'Goonj', 'New Delhi', '123456', 2),
+(3, 'abc@cry.in', '5f4dcc3b5aa765d61d8327deb882cf99', 'Cry', 'Gweyer Hall', '123433', 1);
 
 -- --------------------------------------------------------
 
@@ -108,18 +113,33 @@ CREATE TABLE `student` (
   `email` varchar(40) NOT NULL,
   `college` text NOT NULL,
   `phone` int(10) NOT NULL,
-  `fk_ngo_id` int(11) DEFAULT NULL
+  `Resume` varchar(30) NOT NULL,
+  `fk_ngo_id` int(11) DEFAULT NULL,
+  `fk_internship_details_select_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `password`, `firstname`, `surname`, `email`, `college`, `phone`, `fk_ngo_id`) VALUES
-(1, 'ae2b1fca515949e5d54fb22b8ed95575', 'Raghav', 'Singh', 'raghav97singh.rs@gmail.com', 'Cluster Innovation Centre', 2147483647, 1),
-(2, 'ae2b1fca515949e5d54fb22b8ed95575', 'Madhav', 'Sharma', 'raghav.cicdu@gmail.com', 'IIT', 2147483647, 2),
-(9, 'ae2b1fca515949e5d54fb22b8ed95575', 'ABC', 'DEF', 'abcde@gmail.com', 'CIC', 2147483647, 2),
-(10, 'ae2b1fca515949e5d54fb22b8ed95575', 'EFG', 'HRF', 'efghr@gmail.com', 'CIC', 2147483647, 1);
+INSERT INTO `student` (`id`, `password`, `firstname`, `surname`, `email`, `college`, `phone`, `Resume`, `fk_ngo_id`, `fk_internship_details_select_id`) VALUES
+(1, '5f4dcc3b5aa765d61d8327deb882cf99', 'Anushtha', 'Kalia', '', '', 0, '', 1, NULL),
+(2, '5f4dcc3b5aa765d61d8327deb882cf99', 'Anushtha', 'Kalia', 'anushthakalia@yahoo.com', 'Cluster Innovation Centre', 2147483647, '', 2, NULL),
+(3, '5f4dcc3b5aa765d61d8327deb882cf99', 'Arjun', 'Sharma', 'arjun@gmail.com', 'CIC', 2147483647, '', 1, NULL),
+(11, '5f4dcc3b5aa765d61d8327deb882cf99', 'raghav', 'sing', 'raghav.singh@gmail.com', 'CIC', 2147483647, '', NULL, NULL),
+(12, '5f4dcc3b5aa765d61d8327deb882cf99', 'Raghav', 'Singh', 'raghav@gmail.com', 'cic', 123456789, '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_internship_apply`
+--
+
+CREATE TABLE `student_internship_apply` (
+  `apply_id` int(11) NOT NULL,
+  `fk_intern_id` int(11) NOT NULL,
+  `fk_internship_details_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -137,7 +157,7 @@ ALTER TABLE `company`
 --
 ALTER TABLE `internship_details`
   ADD PRIMARY KEY (`internship_id`),
-  ADD KEY `fk_intern_id` (`fk_intern_id`),
+  ADD KEY `fk_intern_id` (`fk_intern_select_id`),
   ADD KEY `fk_ngo_id` (`fk_ngo_id`);
 
 --
@@ -152,7 +172,16 @@ ALTER TABLE `ngo`
 --
 ALTER TABLE `student`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_ngo_id` (`fk_ngo_id`);
+  ADD KEY `fk_ngo_id` (`fk_ngo_id`),
+  ADD KEY `fk_internship_details_select_id` (`fk_internship_details_select_id`);
+
+--
+-- Indexes for table `student_internship_apply`
+--
+ALTER TABLE `student_internship_apply`
+  ADD PRIMARY KEY (`apply_id`),
+  ADD KEY `fk_internship_details_id` (`fk_internship_details_id`),
+  ADD KEY `fk_intern_id` (`fk_intern_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -162,25 +191,31 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `comid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `comid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `internship_details`
 --
 ALTER TABLE `internship_details`
-  MODIFY `internship_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `internship_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `ngo`
 --
 ALTER TABLE `ngo`
-  MODIFY `ngoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ngoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `student_internship_apply`
+--
+ALTER TABLE `student_internship_apply`
+  MODIFY `apply_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -196,7 +231,7 @@ ALTER TABLE `company`
 -- Constraints for table `internship_details`
 --
 ALTER TABLE `internship_details`
-  ADD CONSTRAINT `internship_details_ibfk_1` FOREIGN KEY (`fk_intern_id`) REFERENCES `student` (`id`),
+  ADD CONSTRAINT `internship_details_ibfk_1` FOREIGN KEY (`fk_intern_select_id`) REFERENCES `student` (`id`),
   ADD CONSTRAINT `internship_details_ibfk_2` FOREIGN KEY (`fk_ngo_id`) REFERENCES `ngo` (`ngoid`);
 
 --
@@ -209,7 +244,15 @@ ALTER TABLE `ngo`
 -- Constraints for table `student`
 --
 ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`fk_ngo_id`) REFERENCES `ngo` (`ngoid`);
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`fk_ngo_id`) REFERENCES `ngo` (`ngoid`),
+  ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`fk_internship_details_select_id`) REFERENCES `internship_details` (`internship_id`);
+
+--
+-- Constraints for table `student_internship_apply`
+--
+ALTER TABLE `student_internship_apply`
+  ADD CONSTRAINT `student_internship_apply_ibfk_1` FOREIGN KEY (`fk_internship_details_id`) REFERENCES `internship_details` (`internship_id`),
+  ADD CONSTRAINT `student_internship_apply_ibfk_2` FOREIGN KEY (`fk_intern_id`) REFERENCES `student` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
